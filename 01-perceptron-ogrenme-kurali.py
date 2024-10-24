0ac956631497e0540a23bdeee5a44e714ed87d60
Gökhan <63648396+gokhangokcen1@users.noreply.github.com>
2024-10-25 00:29:09 +0300

    Update 01-perceptron-ogrenme-kurali.py

--- 01-perceptron-ogrenme-kurali.py
+++ 01-perceptron-ogrenme-kurali.py
@@ -6,35 +6,42 @@ w = [4, 4]
 eta = 1
 teta = -1.5  
 
+"""
+AND KAPISI İÇİN DEĞERLER
+
+x1 = [0, 0, 1, 1]
+x2 = [0, 1, 0, 1]
+hedef_cikti = [0, 0, 0, 1] 
+
+w = [0.25, -0.75]  
+eta = 0.5  
+teta = -0.5  
+"""
+
 def aktivasyon_fonksiyonu(x):
-    if x < 0:
-        return 0
-    else:
-        return 1
+    return 1 if x >= 0 else 0
 
 def cikti(i):
     return aktivasyon_fonksiyonu(w[0] * x1[i] + w[1] * x2[i] + teta)
 
 def guncelle(i, cikti_degeri):
-    global teta  
-    w[0] = w[0] + eta * (hedef_cikti[i] - cikti_degeri) * x1[i]
-    w[1] = w[1] + eta * (hedef_cikti[i] - cikti_degeri) * x2[i]
-    teta = teta + eta * (hedef_cikti[i] - cikti_degeri)  
+    global teta
+    error = hedef_cikti[i] - cikti_degeri
+    w[0] += eta * error * x1[i]
+    w[1] += eta * error * x2[i]
+    teta += eta * error
 
-for i in range(len(x1)):  
-    cikti_degeri = cikti(i) 
-#    print(f"cikti({i}): {cikti_degeri}")
-    if cikti_degeri != hedef_cikti[i]:
-#        print("Hata var")
-        guncelle(i, cikti_degeri)  
-#        print(f"Güncellendi\nw: {w}\nteta: {teta}\n") 
-    else:
-        continue
 
-if w[0] == 0:
-    print(f"Optimum değerler:\nw1 = {w[0]}\nw2 = {w[1]}\nteta = {teta}\ny = aktivasyon_fonksiyonu({w[1]}*x2{teta})")
-elif w[1] == 0:
-    print(f"Optimum değerler:\nw1 = {w[0]}\nw2 = {w[1]}\nteta = {teta}\ny = aktivasyon_fonksiyonu({w[0]}*x1{teta})")
-elif teta == 0:
-    print(f"Optimum değerler:\nw1 = {w[0]}\nw2 = {w[1]}\nteta = {teta}\ny = aktivasyon_fonksiyonu({w[0]}*x1 +{w[1]}*x2)")
+total_error = 1  
+while total_error != 0:
+    total_error = 0  
+    for i in range(len(x1)):
+        cikti_degeri = cikti(i)
+        if cikti_degeri != hedef_cikti[i]:
+            guncelle(i, cikti_degeri)
+            total_error += 1  
+    
+    #print(f"Ağırlıklar [w1,w2]: {w}, Teta: {teta}, Total Errors: {total_error}")
 
+# Optimum ağırlıklar ve teta değeri
+print(f"Optimum değerler:\nw1 = {w[0]}\nw2 = {w[1]}\nteta = {teta}")
